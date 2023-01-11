4c390a41dbc7b824f580aefa3e90fad697f17e8a
lapo <morlap@t-online.de>
2023-01-11 16:12:28 +0100

    fix btn alignment

--- includes/templates/hometrial-button.php
+++ includes/templates/hometrial-button.php
@@ -6,7 +6,7 @@
     data-switch-text="<?php if($show_text) { echo esc_attr( $added ? $button_text : $button_added_text, 'hometrial'); }?>" 
     data-product_id="<?php echo esc_attr( $product_id ); ?>">
         <?php echo $button_icon; ?>
-        <span class="hometrial-btn-text"><?php if($show_text) {echo $added ? $button_added_text : $button_text; }?></span>
+        <?php if($show_text) {echo '<span class="hometrial-btn-text">'; echo $added ? $button_added_text : $button_text; echo '</span>'; }?>
     <div
         class="hometrial-limit-pop">
             <?php echo $max_num_reached_msg; ?>
